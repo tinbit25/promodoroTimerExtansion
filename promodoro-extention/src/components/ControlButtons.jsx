f2f6296b17b50f319edab451de473f0e205424e2
tinbit25 <tinbitelias25@gmail.com>
2025-01-05 03:09:42 +0300

    adjust btn style

--- promodoro-extention/src/components/ControlButtons.jsx
+++ promodoro-extention/src/components/ControlButtons.jsx
@@ -8,18 +8,20 @@ const ControlButtons = ({
   focusTimeTotal
 }) => (
   <div className="flex space-x-4 justify-between">
-    <button
-      onClick={handleStartPause}
-      className={`px-6 py-3 rounded-full text-white font-bold transition-all duration-300 transform hover:scale-105 ${isRunning ? "bg-yellow-500 hover:bg-yellow-400" : "bg-green-500 hover:bg-green-400"}`}
-    >
-      {isRunning ? "Pause" : "Start"}
-    </button>
-    <button
-      onClick={handleRestart}
-      className="px-6 py-3 rounded-full text-white font-bold bg-red-500 hover:bg-red-400 transition-all duration-300 transform hover:scale-105"
-    >
-      Restart
-    </button>
+   <button
+  onClick={handleStartPause}
+  className={`px-6 py-3 rounded-lg text-indigo-500 font-bold transition-all duration-300 transform hover:scale-105 
+    ${isRunning ? "bg-gradient-to-r from-amber-200 to-amber-300 hover:bg-gradient-to-r hover:from-amber-300 hover:to-amber-200" : "bg-gradient-to-r from-emerald-200 to-green-400 hover:bg-gradient-to-r hover:from-green-400 hover:to-emerald-300"}
+    shadow-lg`}
+>
+  {isRunning ? "Pause" : "Start"}
+</button>
+<button
+  onClick={handleRestart}
+  className="px-6 py-3 rounded-lg text-indigo-500 font-bold bg-gradient-to-r from-orange-300 to-orange-500 hover:bg-gradient-to-r hover:from-orange-500 hover:to-orange-300 transition-all duration-300 transform hover:scale-105 shadow-lg"
+>
+  Restart
+</button>
     
     {/* Focus Time Progress */}
     {focusTimeProgress < focusTimeTotal && (
--- promodoro-extention/src/components/TabButton.jsx
+++ promodoro-extention/src/components/TabButton.jsx
@@ -5,7 +5,7 @@ const TabButton = ({ tab, activeTab, setActiveTab }) => (
   <button
     onClick={() => setActiveTab(tab.value)}
     aria-selected={activeTab === tab.value}
-    className={`px-6 py-3 text-lg font-semibold transition-all duration-300 rounded-lg ${activeTab === tab.value ? "border-b-4 border-blue-500 text-blue-500 bg-gradient-to-r from-indigo-100 via-blue-200 to-indigo-300" : "text-gray-0 hover:text-blue-900 hover:bg-indigo-50"}`}
+    className={`px-6 py-3 text-lg font-semibold transition-all duration-300 rounded-lg ${activeTab === tab.value ? "border-b-4 border-blue-500 text-indigo-500 bg-gradient-to-r from-indigo-100 via-blue-200 to-indigo-300" : "text-gray-0 hover:text-blue-900 hover:bg-indigo-50"}`}
   >
     {tab.label}
   </button>
